e63d417128bc9d3d666e56148ca052bd2b75cbd3
Theo Le Bever <theolebever@gmail.com>
2020-03-21 12:44:04 +0100

    sorry forgot to make clean

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.c source/sdl/our_sdl.h source/segmentation/segmentation.h \
- source/process/process.h source/process/../sdl/our_sdl.h \
- source/matrix/matrix.h source/sigmoid/sigmoid.h
